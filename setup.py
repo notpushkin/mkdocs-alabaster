e5028fe5838dfa756cef9f24dc984eaa2171ce11
Alexander Pushkov <alexander@notpushk.in>
2018-08-16 11:41:32 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,18 +2,12 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-__version__ = "0.7.4"
+__version__ = "0.7.5"
 
 if sys.argv[-1] == 'publish':
-    if os.system("pip3 freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip3 install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip3 freeze | grep twine"):
-        print("twine not installed.\nUse `pip3 install twine`.\nExiting.")
-        sys.exit()
-    os.system("rm -r dist/*")
     os.system("python3 setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
+    os.system("rm -r dist/*")
     print("You probably want to also tag the version now:")
     print("  git tag -a {0} -m 'version {0}'".format(__version__))
     print("  git push --tags")
@@ -22,10 +16,10 @@ if sys.argv[-1] == 'publish':
 setup(
     name="mkdocs-alabaster",
     version=__version__,
-    author="Ale",
-    author_email="hi@ale.rocks",
+    author="Alexander Pushkov",
+    author_email="alexander@notpushk.in",
     description="Alabaster port for MkDocs",
-    url="https://github.com/iamale/mkdocs-alabaster",
+    url="https://github.com/notpushkin/mkdocs-alabaster",
     license="BSD",
     packages=find_packages(),
     include_package_data=True,
