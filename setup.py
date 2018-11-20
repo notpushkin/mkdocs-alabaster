eaae4ba6942497d505dbec638789a96a4adf660d
Alexander Pushkov <alexander@notpushk.in>
2018-11-20 15:13:30 +0300

    Bump version (0.7.6)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-__version__ = "0.7.5"
+__version__ = "0.7.6"
 
 if sys.argv[-1] == 'publish':
     os.system("python3 setup.py sdist bdist_wheel")
